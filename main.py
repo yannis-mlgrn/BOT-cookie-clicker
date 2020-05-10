b7514293efd566bb209bb4e692cbdfbf691a5e7e
yannis-mlgrn <yannismlgorn@gmail.com>
2020-05-10 18:50:08 +0200

    ajout du fichier requirements.txt et petite modification de main.py suite a un bug

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
-#import the web driver of google chrome
 WEBDRIVER_PATH= 'bin/chromedriver'
 driver = webdriver.Chrome(executable_path=WEBDRIVER_PATH)
-# load the clicker cookie page
+# suite
 driver.get('https://orteil.dashnet.org/cookieclicker/')
+# sleep(15)
+
+# attendre fin du chargement et init de la page web
+wait = WebDriverWait(driver, 90)
+wait.until(expected_conditions.element_to_be_clickable((By.ID, "bigCookie")))
+
+# suite
 
 while True :
+	cookie = driver.find_element_by_id("bigCookie")
+	cookie.click()
 
-    # wait for loading and init of the web page
-    wait = WebDriverWait(driver, 90)
-    #wait until the cookie is clickable
-    wait.until(expected_conditions.element_to_be_clickable((By.ID, "bigCookie")))
-
-    # bot
-    cookie = driver.find_element_by_id("bigCookie")
-    cookie.click()
+	# récupération du compteur de cookies
+	cookieNum = driver.find_element_by_id("cookies")
+	print("cookies: ", cookieNum.text)
 
 
